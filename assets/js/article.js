5dbbdb6089df1ba7a3b1d815b8a84ec7270cb5bd
Said van de Klundert <said.van.de.klundert@gmail.com>
2023-03-22 17:07:12 +0100

    idk what I am doing

---
+++ assets/js/article.js
@@ -0,0 +1,16 @@
+const articles = [
+    "hello-world",
+    "variables",
+    "debugging",
+    "arrays",
+  ];         
+  let aLen = articles.length;
+
+  let text = "<ul>";
+  for (let i = 0; i < aLen; i++) {
+    text += "<li>" + "<a href='https://saidvandeklundert.net/fortheloveofzig/"+ articles[i] + "'>"+  articles[i] + "</li>";
+    
+  }
+  text += "</ul>";
+
+  document.getElementById("indextag").innerHTML = text;
