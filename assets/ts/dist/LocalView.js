83ff283cca11120e3f5db3cee33995d01005573b
Said van de Klundert <said.van.de.klundert@gmail.com>
2023-03-29 15:39:43 +0200

    adding

--- assets/ts/dist/LocalView.js
+++
@@ -1,8 +0,0 @@
-// utility to display what will be rendered on the web page
-// tsc
-// node .\dist\LocalView.js
-import { Articles, renderArticleIndex } from "./ArticleBuild";
-console.log(Articles);
-const text = renderArticleIndex(Articles);
-console.log(text);
-//# sourceMappingURL=LocalView.js.map
