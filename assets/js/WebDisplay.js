1caab8761e155e73e88872c0581e95620f825c0c
Said van de Klundert <said.van.de.klundert@gmail.com>
2023-03-29 21:37:18 +0200

    adding

--- assets/js/WebDisplay.js
+++ assets/js/WebDisplay.js
@@ -1,9 +1,6 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // Render what is ultimately used in the web page.
-const ArticleBuild_1 = require("./ArticleBuild");
-/*
-const Articles: Array<string> = [
+//import { Articles, renderArticleIndex } from "./ArticleBuild";
+const Articles = [
     "hello-world",
     "variables",
     "debugging",
@@ -20,25 +17,16 @@ const Articles: Array<string> = [
     "naming conventions",
     "source files are structures",
     "more learning resources",
-]
-
-
-
-
-function renderArticleIndex(articles: Array<string>): string{
+];
+function renderArticleIndex(articles) {
     const articleLength = articles.length;
-
-    var text:string = "<ul>";
-    
+    var text = "<ul>";
     for (let i = 0; i < articleLength; i++) {
-        text += "<li>" + "<a href='https://saidvandeklundert.net/fortheloveofzig/"+ articles[i] + "'>"+  articles[i] + "</li>";
+        text += "<li>" + "<a href='https://saidvandeklundert.net/fortheloveofzig/" + articles[i] + "'>" + articles[i] + "</li>";
     }
-    
     text += "</ul>";
     return text;
-    
 }
-*/
-const text = (0, ArticleBuild_1.renderArticleIndex)(ArticleBuild_1.Articles);
+const text = renderArticleIndex(Articles);
 document.getElementById("indextag").innerHTML = text;
 //# sourceMappingURL=WebDisplay.js.map
