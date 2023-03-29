ea28cf083fe93c1413a91ccf731e08e877d88235
Said van de Klundert <said.van.de.klundert@gmail.com>
2023-03-29 20:34:47 +0200

    add

--- assets/js/WebDisplay.js
+++ assets/js/WebDisplay.js
@@ -1,7 +1,32 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // Render what is ultimately used in the web page.
-const ArticleBuild_1 = require("./ArticleBuild");
-const text = (0, ArticleBuild_1.renderArticleIndex)(ArticleBuild_1.Articles);
+// import { Articles, renderArticleIndex } from "./ArticleBuild";
+const Articles = [
+    "hello-world",
+    "variables",
+    "debugging",
+    "arrays",
+    "if",
+    "for",
+    "testing",
+    "string hashmap",
+    "slices",
+    "optionals",
+    "defer",
+    "functions",
+    "structs",
+    "naming conventions",
+    "source files are structures",
+    "more learning resources",
+];
+function renderArticleIndex(articles) {
+    const articleLength = articles.length;
+    var text = "<ul>";
+    for (let i = 0; i < articleLength; i++) {
+        text += "<li>" + "<a href='https://saidvandeklundert.net/fortheloveofzig/" + articles[i] + "'>" + articles[i] + "</li>";
+    }
+    text += "</ul>";
+    return text;
+}
+const text = renderArticleIndex(Articles);
 document.getElementById("indextag").innerHTML = text;
 //# sourceMappingURL=WebDisplay.js.map
