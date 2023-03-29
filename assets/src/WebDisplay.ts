0bcc07dea8620a507cd62e6e80db4e85bf4e8d9b
Said van de Klundert <said.van.de.klundert@gmail.com>
2023-03-29 20:34:38 +0200

    add

--- assets/src/WebDisplay.ts
+++ assets/src/WebDisplay.ts
@@ -1,6 +1,40 @@
 // Render what is ultimately used in the web page.
-import { Articles, renderArticleIndex } from "./ArticleBuild";
+// import { Articles, renderArticleIndex } from "./ArticleBuild";
+const Articles:Array<string> = [
+    "hello-world",
+    "variables",
+    "debugging",
+    "arrays",
+    "if",
+    "for",
+    "testing",
+    "string hashmap",
+    "slices",
+    "optionals",
+    "defer",
+    "functions",    
+    "structs",
+    "naming conventions",
+    "source files are structures",
+    "more learning resources",
+]
 
+
+
+
+function renderArticleIndex(articles: Array<string>): string{
+    const articleLength = articles.length;
+
+    var text:string = "<ul>";
+    
+    for (let i = 0; i < articleLength; i++) {
+        text += "<li>" + "<a href='https://saidvandeklundert.net/fortheloveofzig/"+ articles[i] + "'>"+  articles[i] + "</li>";
+    }
+    
+    text += "</ul>";
+    return text;
+    
+}
 
 const text = renderArticleIndex(Articles);
 document.getElementById("indextag").innerHTML = text;
