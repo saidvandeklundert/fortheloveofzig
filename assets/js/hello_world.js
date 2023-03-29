fa5ce352b566b9e5a6afc05ed54bee0de5d58fc3
Said van de Klundert <said.van.de.klundert@gmail.com>
2023-03-29 15:10:37 +0200

    adding

--- assets/js/hello_world.js
+++
@@ -1,4 +0,0 @@
-// ctrl + shift + b to compile
-var message = "Hello world!";
-console.log(message);
-//# sourceMappingURL=hello_world.js.map
--- assets/js/hello_world.ts
+++
@@ -1,3 +0,0 @@
-// ctrl + shift + b to compile
-let message: string = "Hello world!";
-console.log(message);
